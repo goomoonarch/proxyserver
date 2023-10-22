54c5bf15c7b5847058de07eb0525bd7d1592b628
Jhoss <jhosser@live.com>
2023-10-22 14:50:02 -0500

    add enviorement variables

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 
+
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
